ea577f72b31e8e4ab799b56bfefc7cc313a492e1
root <root@20225233-p02.localdomain>
2024-03-13 17:42:00 +0900

    ..

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ import tensorflow as tf
 
 parser = argparse.ArgumentParser(description='Point Source Removal Deep Learning')
 
-#TRAIN-SET
-parser.add_argument('input_path', type=str, default='./', help='Input Path')
-parser.add_argument('output_path', type=str, default='./', help='Output Path')
+#Required
+parser.add_argument('input_path', type=str, help='Input Path')
+parser.add_argument('output_path', type=str, help='Output Path')
 
-#TRAIN
+#TRAIN-SET
 parser.add_argument('--making_train_set', type=bool, default=False, help='If you want to make training set : True')
 parser.add_argument('--number_of_trainset', type=int, default=2560, help='Number of Training sets')
 parser.add_argument('--agumentation', type=bool, default=True, help='Rotational Agumentation?(Number of Training sets x 4) : True')
 parser.add_argument('--background', type=bool, default=False, help='If you want to make background cube : True')
 parser.add_argument('--making_test_data', type=bool, default=False, help='If you want to make SDC3 testdata : True')
+parser.add_argument('--frequency_channels',type=int,default=264,help='How many channels are you goint to use? / 1channel : 0.1MHz')
 
-
-
-parser.add_argument('--epoch', type=int, default=10, help='Epoch')
-parser.add_argument('--batch_size', type=int, default=10, help='Epoch')
+#TRAINING
+parser.add_argument('--epochs', type=int, default=10, help='Epoch')
+parser.add_argument('--validation_split', type=float, default=0.2, help='Validation Split')
+parser.add_argument('--batch_size', type=int, default=4, help='batch size')
 
 #TEST
 parser.add_argument('--apply_to_SDC3a', type=bool, default=True, help='To apply model to SDC3 : True/False')
@@ -32,5 +33,5 @@ if __name == '__main__':
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
     Mytrainset.process_data(args.output_path,args.number_of_trainset,args.)
-    Mytrain.training()
-    Mytest.testing()
+    Mytrain.training(args.input_path,args.output_path,args.epochs,args.validation_split,args.batch_size)
+    Mytest.test()
