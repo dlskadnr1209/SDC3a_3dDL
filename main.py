b0c8883a17c18d41e1a83a8b2499405fdce95512
dlskadnr1209 <dlskadnr1209@unist.ac.kr>
2024-03-13 15:26:40 +0900

    ..

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ parser.add_argument('input_path', type=str, default='./', help='Input Path')
 parser.add_argument('output_path', type=str, default='./', help='Output Path')
 
 #
-parser.add_argument('--making_background', type=bool, default=False, help='To make background cube : True/False')
-parser.add_argument('--making_test_data', type=bool, default=False, help='To make test data : True/False')
+parser.add_argument('--making_train_set', type=bool, default=False, help='If you want to make training set : True')
+parser.add_argument('--background', type=bool, default=False, help='If you want to make background cube : True')
+parser.add_argument('--making_test_data', type=bool, default=False, help='If you want to make SDC3 testdata : True')
+
+
+
 parser.add_argument('--epoch', type=int, default=10, help='Epoch')
+parser.add_argument('--batch_size', type=int, default=10, help='Epoch')
 
 #
-
+parser.add_argument('--apply_to_SDC3a', type=bool, default=True, help='To apply model to SDC3 : True/False')
 
 if __name == '__main__':
     args=parser.parse_args()
-    
+    Mytrainset
+    Mytrain
+    Mytest
